748ff2c86b8085522307e57e9a0211004236eed0
joelkoz <joel.kozikowski@gmail.com>
2020-07-09 22:51:00 -0400

    Added tech docs on Dragino HAT to RH_LoRa class

--- RH_LoRa.h
+++ RH_LoRa.h
@@ -3,6 +3,8 @@
 // Definitions for SX1276 based radios in LoRa mode. 
 // This code is a mixture of the original RH_RF95 RadioHead code
 // as well as LoRa code found in ESP32 based LoRa code for Heltec's WiFi Lora 32 V2 MCU board.
+// This code allows for the Dragino HAT to work, unmodified, on Raspian Buster and beyond,
+// as well as communicate with Heltec Lora 32 boards.
 // 
 #ifndef RH_LORA_h
 #define RH_LORA_h
@@ -67,7 +69,56 @@
 /// capable radio transceiver.
 ///
 /// For Semtech SX1276 chip found in Heltec ESP32 LoRa V2 and Dragino LoRa HAT
+/// This class has been tested with an unmodified Dragino Lora HAT on a Raspberry Pi 3
+/// with Raspian Buster as the OS. It has also been tested with a Heltec Wifi Lora 32 V2
+/// board.
+/// Getting the Semtech SX1276 chip to work on these boards require a functioning SPI
+/// system that allows the "Slave select" pin (aka SS, NSS, Chip Select, CS) to be
+/// specified, along with the pin connected to the SX1276 DIO0 pin and the pin
+/// connected to the SX1276 reset pin.  It is important to note that the SS pin
+/// on the Dragino Lora HAT (even at board version 1.4) is NOT connected to the standard
+/// pin used by Raspian's kernel SPI implementation. That is the primary reason this
+/// driver was written (vs. a hardware hack to get this to work with RH_RF95 driver).
 ///
+/// For Raspberry Pi, there are THREE types of pin numbering systems floating around:
+///   1. The pin system used by the Bcm2835 library
+///   2. The pin system used by the WiringPi library
+///   3. The actual pin numbers found on the GPIO header board
+/// This class uses the WiringPi library for its implementation, but WiringPi can be
+/// initialized with any one of the above systems, depending on which "setup" method is
+/// called.  See http://wiringpi.com/reference/setup/ for details.
+/// 
+/// For reference, here are pin numbers used by both the Dragino Hat and the Heltec MCU
+/// when connected to their respective SX1276 chips:
+///
+/// Dragino LoRa HAT to RPi pin wiring:
+/// http://www.dragino.com/downloads/downloads/LoRa-GPS-HAT/LoRa_GPS_HAT_UserManual_v1.0.pdf
+/// BCM25/wPi6 (22) = Lora NSS (SPI slave select)
+/// BCM17/wPi0 (11) = Lora Reset
+/// BCM4/wPi7 (7) = Lora DIO0 (aka Interupt Pin in RadioHead's RH_RF95.h)
+/// BCM9/wPi13 (21) = Lora/RPiSPI0 MISO
+/// BCM10/wPi12 (19) = Lora/RPiSPI0 MOSI
+/// BCM11/wPi14 (23) = Lora/RPiSPI0 SCLK
+/// BCM23/wPi4 (16) = DIO1
+/// BCM24/wPi5 (18) = DIO2
+/// BCM18/wPi1 (12) = 1PPS
+/// 
+/// NSS is SPI abbreviation for "Slave Select" pin. Thus above, that pin is 25 in BCM numbering, 6 in
+/// WiringPi number, and physical pin 22 on the Dragino Board/RasPi Header pins.
+/// 
+/// DIO = "Digital I/O" pin from LoRa chip. They can be assigned different functions via software.
+/// This class uses DIO0 as an interrupt pin for Tx/Rx done
+/// 
+/// Heltec LoRa pin connections
+/// (19) = Lora_MISO
+/// (27) = Lora_MOSI
+///  (5) = Lora_SCLK
+/// (14) = Lora reset
+/// (18) = Lora NSS (Slave select)
+/// (26) = Lora DIO0
+/// (35) = Lora DIO1
+/// (34) = Lora DIO2
+/// 
 
 class RH_LoRa : public RHSPIDriver
 {
